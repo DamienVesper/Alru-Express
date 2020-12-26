51cfed44d5ac38e0d6fafd664bb485b9a016edf5
Damien Vesper <ldamienvesper@gmail.com>
2020-12-25 22:28:36 -0600

    no need for https

---
+++ config.js
@@ -0,0 +1,4 @@
+let config = {
+    port: 8777,
+    
+}
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const dotenv = require(`dotenv`).config();
+const config = require(`./config.js`);
+
 const express = require(`express`);
 const app = express();
 
-app.listen(`zeropoint.bruh`)
+
+
+app.listen(config.port, () => console.log(`Server is listneing at port bruh moment`)
+});
