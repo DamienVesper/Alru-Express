356c78eb700eef46bfe8c67d15477282fb756f83
Damien Vesper <ldamienvesper@gmail.com>
2020-12-31 19:53:21 -0600

    yes to site

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 router.get(`/`, (req, res, next) => res.render(`index.ejs`));
 router.get(`/about`, (req, res, next) => res.render(`about.ejs`));
-router.get(`/minecraft`, (req, res, next) => res.render(`minecraft.ejs`));
 
 module.exports = router;
