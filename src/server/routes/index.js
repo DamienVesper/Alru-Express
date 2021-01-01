b51b91dfe2e1dfd36df1cb0738a8508f51a40ca6
Damien Vesper <ldamienvesper@gmail.com>
2020-12-31 19:35:42 -0600

    remove iw4m

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.get(`/`, (req, res, next) => res.render(`index.ejs`));
 router.get(`/about`, (req, res, next) => res.render(`about.ejs`));
-router.get(`/iw4m`, (req, res, next) => res.render(`iw4m.ejs`));
 router.get(`/minecraft`, (req, res, next) => res.render(`minecraft.ejs`));
 
 module.exports = router;
