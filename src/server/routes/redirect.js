ecb2ae09f3bc9befc5841874d3ac9dce0f3f4b4b
ZEROPOINT IS A TANK <50062288+ZEROPOINTBRUH@users.noreply.github.com>
2021-01-31 10:53:36 -0700

    Touching up the site
    There will be Stuff added here soon

--- src/server/routes/redirect.js
+++ src/server/routes/redirect.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get(`/discord`, (req, res, next) => res.redirect(`https://discord.io/AllianceReunited`));
 router.get(`/admin`, (req, res, next) => res.redirect(`https://www.youtube.com/watch?v=xvFZjo5PgG0`));
+router.get(`/topsecret`, (req, res, next) => res.redirect(`https://sfx.alru.ga/secret.html`));
 router.get(`/minecraft`, (req, res, next) => res.redirect(`https://map.fortressmc.cf`));
 router.get(`/iw4m`, (req, res, next) => res.redirect(`https://pw.alru.ga`));
 router.get(`/sfx`, (req, res, next) => res.redirect(`https://sfx.alru.ga`));
