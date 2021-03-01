f56f79d944c37a3186f84cc868f8a5b5431977c2
ZEROPOINT IS A TANK <50062288+ZEROPOINTBRUH@users.noreply.github.com>
2021-03-01 11:31:45 -0700

    Update redirect.js

--- src/server/routes/redirect.js
+++ src/server/routes/redirect.js
@@ -7,5 +7,8 @@ router.get(`/minecraft`, (req, res, next) => res.redirect(`https://map.fortressm
 router.get(`/iw4m`, (req, res, next) => res.redirect(`https://pw.alru.ga`));
 router.get(`/sfx`, (req, res, next) => res.redirect(`https://sfx.alru.ga`));
 router.get(`/sharex`, (req, res, next) => res.redirect(`https://share.alru.ga`));
+router.get(`/speed`, (req, res, next) => res.redirect(`https://speed.alru.ga`));
+router.get(`/secret`, (req, res, next) => res.redirect(`https://alru.ga/discord`));
+router.get(`/share`, (req, res, next) => res.redirect(`https://share.alru.ga`));
 
 module.exports = router;
