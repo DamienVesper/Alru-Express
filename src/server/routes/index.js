3d0b2e90f710df0609a027ad1600caf90f01925e
ZEROPOINT IS A TANK <50062288+ZEROPOINTBRUH@users.noreply.github.com>
2021-03-01 12:09:45 -0700

    A Secret Page that was not intended to surprise anyone

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.get(`/`, (req, res, next) => res.render(`index.ejs`));
 router.get(`/about`, (req, res, next) => res.render(`about.ejs`));
+router.get(`/secret`, (req, res, next) => res.render(`secret.ejs`));
 
 module.exports = router;
--- src/server/routes/redirect.js
+++ src/server/routes/redirect.js
@@ -8,7 +8,6 @@ router.get(`/iw4m`, (req, res, next) => res.redirect(`https://pw.alru.ga`));
 router.get(`/sfx`, (req, res, next) => res.redirect(`https://sfx.alru.ga`));
 router.get(`/sharex`, (req, res, next) => res.redirect(`https://share.alru.ga`));
 router.get(`/speed`, (req, res, next) => res.redirect(`https://speed.alru.ga`));
-router.get(`/secret`, (req, res, next) => res.redirect(`https://alru.ga/discord`));
 router.get(`/share`, (req, res, next) => res.redirect(`https://share.alru.ga`));
 
 module.exports = router;
