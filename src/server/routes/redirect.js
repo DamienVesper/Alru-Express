ba915082529afc479abf8d248e3028f00a9f9a57
ZEROPOINT <wegj1@hotmail.com>
2021-04-20 15:14:24 -0600

    :trollface: we will be changing this soon

--- src/server/routes/redirect.js
+++ src/server/routes/redirect.js
@@ -3,11 +3,10 @@ const router = express.Router();
 
 router.get(`/discord`, (req, res, next) => res.redirect(`https://discord.io/AllianceReunited`));
 router.get(`/admin`, (req, res, next) => res.redirect(`https://www.youtube.com/watch?v=xvFZjo5PgG0`));
-router.get(`/minecraft`, (req, res, next) => res.redirect(`https://map.fortressmc.cf`));
-router.get(`/iw4m`, (req, res, next) => res.redirect(`https://pw.alru.ga`));
-router.get(`/sfx`, (req, res, next) => res.redirect(`https://sfx.alru.ga`));
+router.get(`/minecraft`, (req, res, next) => res.redirect(`minecraft://mc.alru.ga`));
 router.get(`/sharex`, (req, res, next) => res.redirect(`https://share.alru.ga`));
 router.get(`/speed`, (req, res, next) => res.redirect(`https://speed.alru.ga`));
 router.get(`/share`, (req, res, next) => res.redirect(`https://share.alru.ga`));
+router.get(`/panel`, (req, res, next) => res.redirect(`https://panel.alru.ga`));
 
 module.exports = router;
