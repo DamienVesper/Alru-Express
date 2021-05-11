44cddd4f82be59e0c79043a8f82e60630d4ba686
ZEROPOINT <wegj1@hotmail.com>
2021-05-11 17:40:16 -0600

    im learning typescript

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,30 @@
 import config from './config/config';
 import log from './utils/log';
 
-import * as http from 'http';
+import * as HTTP from 'http';
 import * as Express from 'express';
 
 import pageRouter from './routes/index';
 import redirectRouter from './routes/redirect';
 import apiRouter from './routes/api';
 
+import * as path from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express.Application = Express();
 
+const server: HTTP.Server = HTTP.createServer(app);
+server.listen(config.port, () => log(`green`, `Server is listening at port ${config.port}.`))
+
+app.set(`views`, path.resolve(__dirname, `views`));
+app.set(`view engine`, `ejs`);
+
+app.use(`/assets`, Express.static(path.resolve(__dirname, `../client/assets`)));
+
+app.use
+
 export default {
-    app
+    app,
+    server
 };
