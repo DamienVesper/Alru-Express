6f6972797b4a60234074ddad572954f718aa6b33
ZEROPOINT <wegj1@hotmail.com>
2021-05-24 19:55:20 -0600

    remove all the old stuff

--- _src/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require(`express`);
-const router = express.Router();
-
-router.get(`/`, (req, res, next) => res.render(`index.ejs`));
-router.get(`/about`, (req, res, next) => res.render(`about.ejs`));
-router.get(`/secret`, (req, res, next) => res.render(`secret.ejs`));
-
-module.exports = router;
