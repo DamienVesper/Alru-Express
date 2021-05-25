8e9a830bfe528854695706a3b4f20540ddcdbdf6
ZEROPOINT <wegj1@hotmail.com>
2021-05-24 19:08:52 -0600

    finish up the build sequences

--- build/src/server/routes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const indexRouter = express.Router();
-exports.default = indexRouter;
