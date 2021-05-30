c08808edbd73e51eecebc32c75ef75f08b87a091
Damien Vesper <ldamienvesper@gmail.com>
2021-05-30 16:16:38 -0500

    fix router

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,11 +4,10 @@ import log from './utils/log';
 import * as HTTP from 'http';
 import express from 'express';
 
-// import pageRouter from './routes/index';
-// import apiRouter from './routes/api';
+import indexRouter from './routes/index';
+import apiRouter from './routes/api';
 
 import * as path from 'path';
-import * as fs from 'fs';
 
 const EJSLayouts = require(`express-ejs-layouts`);
 
@@ -21,6 +20,9 @@ app.set(`view engine`, `ejs`);
 app.use(EJSLayouts);
 
 app.use(`/assets`, express.static(path.resolve(__dirname, `../client/assets`)));
+
+app.use(`/api`, apiRouter);
+app.use(`/`, indexRouter);
 
 server.listen(config.port, () => log(`green`, `Server is listening at port ${config.port}.`));
 
