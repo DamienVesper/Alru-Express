8f444aaea69db6712ed0fd42779452ecd89c8979
Damien Vesper <ldamienvesper@gmail.com>
2021-05-30 17:59:10 -0500

    split header, fix file loader

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,7 +9,7 @@ indexRouter.use(`/`, (req: Express.Request, res: Express.Response) => {
     if (req.path === `/`) res.render(`index.ejs`);
     else {
         const pathToFile = req.path.slice(1);
-        const fileExists = fs.existsSync(path.resolve(__dirname, `../client/views`, `${pathToFile}.ejs`));
+        const fileExists = fs.existsSync(path.resolve(__dirname, `../../client/views`, `${pathToFile}.ejs`));
 
         if (fileExists) res.render(`${pathToFile}.ejs`);
         else res.render(`errors/404.ejs`);
